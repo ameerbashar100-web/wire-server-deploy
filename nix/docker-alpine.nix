69e832e322e1f22b3131049d3ce1945a46c8ea50
ameerbashar100-web <ameerbashar100@gmail.com>
2026-09-09 11:43:50 +0300

    Delete nix directory

--- nix/docker-alpine.nix
+++
@@ -1,7 +0,0 @@
-{
-  imageName = "alpine";
-  imageDigest = "sha256:21a3deaa0d32a8057914f36584b5288d2e5ecc984380bc0118285c70fa8c9300";
-  sha256 = "1wmrq8x0l5sjrwlklvfkabmxpn0qphik1gb37i04x8jm8bjiisip";
-  finalImageName = "alpine";
-  finalImageTag = "3";
-}
--- nix/scripts/create-offline-artifact.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p assets assets/containers-{helm,other,system} assets/debs assets/binaries
-
-mirror-apt-jammy assets/debs
-
-
-
